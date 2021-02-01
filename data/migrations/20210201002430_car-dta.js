830a376c1f676afb3680c1a88bec1c8014e19b03
extrude575757 <nicholas.a.lacapria@gmail.com>
2021-02-01 00:44:05 -0800

    Seeds added

--- data/migrations/20210201002430_car-dta.js
+++ data/migrations/20210201002430_car-dta.js
@@ -1,13 +1,14 @@
 
 exports.up = function(knex) {
-  return knex.schema.table('cardta', c=>{
+  return knex.schema.createTable('cardta', c=>{
     c.increments();
-    c.string('name')
+    c.string('name',16);
+    c.string('car-bought',16);
   })
 };
 
 exports.down = function(knex) {
-  return knex.schema.table('cardta', c=>{
+  return knex.schema.dropTableIfExists('cardta', c=>{
       c.dropColumn('name')
   })
 };
